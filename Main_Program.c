7c92a50e5c15c0d6d147ce3bc28df1db68352a4d
Arnan <arnanda.prasatya.tif422@polban.ac.id>
2022-12-05 17:17:14 +0700

    menambahkan prosedur dan fungsi kosong

--- Main_Program.c
+++ Main_Program.c
@@ -8,8 +8,103 @@ typedef struct(){
     char papan[7][7];
 }Papan;
 
+void cetak_menu_awal();
+
+void pilih_pemain();
+
+void pilih_dimensi_papan();
+
+void mulai_permainan();
+
+void cetak_papan();
+
+void giliran_pemain();
+
+void cek_menang();
+
+void cek_menang_3x3();
+
+void cek_menang_5x5();
+
+void cek_menang_7x7();
+
+int cek_tempat_kosong();
+
+void cetak_tampilan_menang();
+
+int ulangi_permainan();
+
+void timer();
+
+void simpan_skor();
+
+void hitung_skor();
 
 int main(){
 
 
+}
+
+void cetak_menu_awal(){
+
+}
+
+void pilih_pemain(){
+
+}
+
+void pilih_dimensi_papan(){
+
+}
+
+void mulai_permainan(){
+
+}
+
+void cetak_papan(){
+
+}
+
+void giliran_pemain(){
+
+}
+
+void cek_menang(){
+
+}
+
+void cek_menang_3x3(){
+
+}
+
+void cek_menang_5x5(){
+
+}
+
+void cek_menang_7x7(){
+
+}
+
+int cek_tempat_kosong(){
+
+}
+
+void cetak_tampilan_menang(){
+
+}
+
+int ulangi_permainan(){
+
+}
+
+void timer(){
+
+}
+
+void simpan_skor(){
+
+}
+
+void hitung_skor(){
+    
 }
