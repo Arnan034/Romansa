b00b3b6f0a4811801efd247c7ff4e3045d2fc9f4
Arnan <arnanda.prasatya.tif422@polban.ac.id>
2022-12-05 17:30:05 +0700

    menambahkan prosedur cek_pemenang ke main_program, ver1

---
+++ cek_pemenang.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void cek_menang();
+
+void cek_menang_3x3();
+
+void cek_menang_5x5();
+
+void cek_menang_7x7();
+
+int main(){
+
+}
+
+void cek_menang(){
+// ini prosedur cek pemenang
+}
+
+void cek_menang_3x3(){
+
+}
+
+void cek_menang_5x5(){
+
+}
+
+void cek_menang_7x7(){
+
+}
